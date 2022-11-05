f4c00a4afee3db2ded81f2dc138dd41e312c9c70
wwszola <wwszola@student.agh.edu.pl>
2022-11-05 21:11:58 +0100

    update for refactor

--- template/callback.py
+++ template/callback.py
@@ -2,13 +2,14 @@ from typing import Iterator
 
 from text_render import TextRender
 
-def _callback(design: TextRender) -> Iterator[bool]:
+def _callback(design: TextRender, user_settings: dict) -> Iterator[bool]:
     """entry point
     make changes to design
     mark end of the each frame with yield True
     return None or yield False to quit
     """
-    for _ in range(8):
+    design.put_words('Hello, world!', (0, 0))
+    while True:
         yield True
-    
+
     return None
