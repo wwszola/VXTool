071bb8e71d05f0acb74cc53f3c7192f6de36f87b
wwszola <wwszola0@gmail.com>
2023-02-05 23:23:17 +0100

    utility words_line and words_bound generators

--- text_render.py
+++ text_render.py
@@ -340,6 +340,27 @@ if __name__ == '__main__':
     _main()
 
 # Utility functions
+def words_line(text: str, pos: tuple[int, int] = (0, 0)) -> Generator:
+    y = pos[1]
+    for i, letter in enumerate(text):
+        x = pos[0] + i
+        yield (x, y), letter
+
+def words_bound(text: str, region: Rect) -> Generator:
+    pos = (0, 0)
+    while len(text) > 0 and pos[1] < region.height:
+        cut = region.width - pos[0]
+        word = text[:cut]
+        text = text[cut:]
+        yield from words_line(
+            word, 
+            (pos[0] + region.left, pos[1] + region.top)
+        )
+        if pos[0] + len(word) >= region.width:
+            pos = (0, pos[1] + 1)
+        else:
+            pos = (pos[0] + len(word), pos[1])
+
 def scroll(text: str, window: int, start: int = 0) -> Iterator[str]:
     """yields views starting from empty view, shifts to left
     returns number of views yield
