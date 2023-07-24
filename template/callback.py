54f2cf0003375e6634939402ebb893ea1635e779
wwszola <wwszola0@gmail.com>
2023-07-24 20:44:56 +0200

    using CallbackProcess, showing event handlers

--- template/callback.py
+++ template/callback.py
@@ -1,79 +1,80 @@
-from typing import Generator
-
-from itertools import cycle
-from math import sin
-
-from VXTool.render import TextRender
-from VXTool.core import Font, Color, Dot, Buffer
+from VXTool.render import TextRender, RENDER_MSG
+from VXTool.core import Color, Dot, Buffer
 from VXTool.util import words_line, line_seq
+from VXTool.app import PickableEvent
+from VXTool.callback import CallbackProcess
 
-def _callback(design: TextRender, user_settings: dict) -> Generator:
-    """entry point
-    first mark end of your setup with yield True
-    then mark end of the each frame with yield True
-    yield False to quit
-    """
-    # UniVGA16: Font = user_settings['fonts']['UniVGA16'][16]
-    base_dot: Dot = Dot(
-        pos = (-1, -1), 
-        letter = '█', 
-        color = Color(100, 70, 140), 
-        font_family = "UniVGA16",  
-        font_size = 16,
-        clear = False
-    )
-    
-    # two lines below are required before drawing anything on design
-    design.block_size = design.get_dot_size(base_dot)
-    design.resize_screen()
+class Callback(CallbackProcess):
+    def setup(self):
+        self.base_dot: Dot = Dot(
+            pos = (-1, -1), 
+            letter = '█', 
+            color = Color(100, 70, 140), 
+            font_family = "UniVGA16",  
+            font_size = 16,
+            clear = False
+        )
+        self.send('TEXT_RENDER', None, RENDER_MSG.ATOMIC_SET_BLOCK_SIZE, self.base_dot)
 
-    colors = user_settings["COLORS"]
+        colors = self.user_settings["COLORS"]
 
-    layer1 = Buffer()
-    texts = [
-        "This is a",
-        "test for ",
-        "colors.  ",
-    ]
-    color = colors["RED"]
-    for i, line in enumerate(texts):
-        layer1.extend((
-            base_dot.variant(pos = pos, letter = letter, color = color)
-            for pos, letter in words_line(line, (1, i + 1))
-    ))
+        layer1 = Buffer()
+        texts = [
+            "This is a",
+            "test for ",
+            "colors.  ",
+        ]
+        color = colors["RED"]
+        for i, line in enumerate(texts):
+            layer1.extend((
+                self.base_dot.variant(pos = pos, letter = letter, color = color)
+                for pos, letter in words_line(line, (1, i + 1))
+        ))
 
-    layer2 = Buffer()
-    texts2 = [
-        "            ",
-        "Is this     ",
-        "transparent?",
-    ]
+        layer2 = Buffer()
+        texts2 = [
+            "            ",
+            "Is this     ",
+            "transparent?",
+        ]
 
-    color = colors["BLUE"]
-    color = Color(color.r, color.g, color.b, 192)
-    for i, line in enumerate(texts2):
-        layer2.extend((
-            base_dot.variant(pos = pos, letter = letter, color = color)
-            for pos, letter in words_line(line, (1, i + 1))
-    ))
+        color = colors["BLUE"]
+        color = Color(color.r, color.g, color.b, 192)
+        for i, line in enumerate(texts2):
+            layer2.extend((
+                self.base_dot.variant(pos = pos, letter = letter, color = color)
+                for pos, letter in words_line(line, (1, i + 1))
+        ))
 
-    layer3 = Buffer()
-    P1, P2 = (3, 2), (13, 6)
-    layer3.extend((
-        base_dot.variant(pos = pos, color = colors["GREEN"])
-        for pos in line_seq(P1, P2)
-    ))
+        layer3 = Buffer()
+        P1, P2 = (3, 2), (13, 6)
+        layer3.extend((
+            self.base_dot.variant(pos = pos, color = colors["GREEN"])
+            for pos in line_seq(P1, P2)
+        ))
 
-    events = yield True
+        self.screen = Buffer()
+        self.screen.merge(layer3)
+        self.screen.merge(layer1)
+        self.screen.merge(layer2)
 
-    # your draw loop
-    while True:
-        design.clear()
+        self.text = 'ABCDEFGHIJK'
 
-        design.draw(layer3)
-        design.draw(layer1)
-        design.draw(layer2)
+    def update(self):
+        self.screen.put(self.base_dot.variant(
+            pos = (3, 3), 
+            letter = self.text[self.updates_count%len(self.text)],
+            clear = True
+        ))
+        self.send('TEXT_RENDER', self.screen)
 
-        events = yield True
+    def on_KEYDOWN_SPACE(self, attrs: dict):
+        self.on_KEYDOWN(attrs)
+        self.text = 'AUTOMATIC'
 
-    yield False
+    def on_KEYDOWN(self, attrs: dict):
+        dot = self.base_dot.variant(clear=True)
+        self.screen.extend(
+            dot.variant(pos = pos, letter = letter)
+            for pos, letter in words_line(attrs['key_name'], (1, 4))
+        )
