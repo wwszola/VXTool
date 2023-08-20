45fd953c119c18c686d1cedbf194957ccd1f9a34
wwszola <wwszola0@gmail.com>
2023-08-20 21:25:39 +0200

    added some comments as guide

--- VXTool/__main__.py
+++ VXTool/__main__.py
@@ -15,10 +15,13 @@ class LAUNCH_MSG(Enum):
     CALLBACK_FAIL = auto()
 
 def _main():
+    # first argument is read as a directory to specified project
     project_dir: Path = Path(argv[1]) 
     out_dir = project_dir / 'out'
     print(project_dir)
 
+    # don't overwrite any of the defaults below
+    # in your project's settings.json file 
     _SETTINGS = OrderedDict([
         ("USER", OrderedDict([
             ("fonts", {}),
@@ -27,6 +30,9 @@ def _main():
         ]))
     ])
     msgs = []
+
+    # tries to load settings and callback files first,
+    # before checking for a command
     settings_path = project_dir / 'settings.json'
     try:
         with open(settings_path, 'r') as file:
@@ -36,12 +42,16 @@ def _main():
     except FileNotFoundError as e:
         msgs.extend((LAUNCH_MSG.NO_SETTINGS_FILE, project_dir))
 
+    # to properly load colors in settings file use "COLORS" attribute
+    # ex. "USER": {"COLORS": {"WHITE": [0, 0, 0]}}
     if "COLORS" in _SETTINGS["USER"]:
         colors = OrderedDict()
         for color_name, rgba in _SETTINGS["USER"]["COLORS"].items():
             colors[color_name] = Color(*rgba)
         _SETTINGS["USER"]["COLORS"] = colors
 
+    # your's callback.py file should include a class named Callback
+    # inheriting from VXTool.callback.CallbackProcess
     try:
         path.append(project_dir.as_posix())
         from callback import Callback
@@ -51,26 +61,29 @@ def _main():
         msgs.extend((LAUNCH_MSG.NO_CALLBACK_FILE, project_dir))
 
     _SETTINGS['TASKS']: dict = {
-        'movie': _movie_task_call,
-        'create': _new_project_task_call
+        'movie': _movie_task_call, # stitch rendered .png files into .mp4 movie
+        'create': _new_project_task_call # creates new project from VXTool_template
     }
-    
+
+    # if you include more than one argument,
+    # the default behaviour i.e. to launch your intended project and
+    # execution of "end_tasks" specified in "APP" settings are ignored
+
+    # second argument is name of the command/task you want to use
     if len(argv) > 2 and argv[2] in _SETTINGS['TASKS'].keys():
         call = _SETTINGS['TASKS'].get(argv[2], None)
         if call: call(_SETTINGS, msgs)
         return msgs
 
-
-    # render_size = _SETTINGS['APP']['render_size']
-    # full_res = _SETTINGS['TEXT_RENDER']['full_res']
-    # shape = _SETTINGS['TEXT_RENDER']['shape']
-
+    # app is able to launch when settings and callback files are loaded
     if LAUNCH_MSG.NO_CALLBACK_FILE in msgs or LAUNCH_MSG.NO_SETTINGS_FILE in msgs:
         msgs.append(LAUNCH_MSG.CALLBACK_FAIL)
         return msgs
 
     _app(_SETTINGS)
 
+    # "end_tasks" may be specified which are commands to be executed
+    # after successful _app launch 
     for task in _SETTINGS['APP']['end_tasks']:
         call = _SETTINGS['TASKS'][task]
         if call: call(_SETTINGS, msgs)
@@ -79,10 +92,14 @@ def _main():
 
 # Tasks
 def _movie_task_str(settings: OrderedDict) -> str:
-    """this is ffmpeg sequence that worked for me"""
-    fps = settings['APP']['FPS']
+    # this is ffmpeg sequence that worked for me
+    # compression, color formats may be adjustable
+    app_fps = settings['APP']['FPS']
+    movie_fps = 60
     out_dir = settings['USER']['out_dir']
-    return f'ffmpeg -framerate {fps} -i ' + (out_dir / f'frame_%05d.png').as_posix() + ' -c:v libx264 -pix_fmt yuv420p -vf scale=out_color_matrix=bt709 -r 60 ' + (out_dir / 'movie.mp4').as_posix()
+    img_path = (out_dir / 'frame_%05d.png').as_posix()
+    movie_path = (out_dir / 'movie.mp4').as_posix()
+    return f'ffmpeg -framerate {app_fps} -i {img_path} -c:v libx264 -pix_fmt yuv420p -vf scale=out_color_matrix=bt709 -r {movie_fps} {movie_path}'
 
 def _movie_task_call(settings: OrderedDict, msgs: list[LAUNCH_MSG], *args):
     if LAUNCH_MSG.NO_SETTINGS_FILE in msgs:
