08820180a944bc513c3f23b61ae4fe3783f78bc8
wwszola <wwszola0@gmail.com>
2023-09-07 19:36:16 +0200

    loop letters util animation

--- VXTool/util.py
+++ VXTool/util.py
@@ -170,3 +170,10 @@ def spell_and_stop(dot: AnimatedDot, delta_time: int = 0, text: str = ''):
     dot.op_set(delta_time, 'letter', text)
     dot.op_stop(delta_time + len(text))
     return dot
+
+def loop_letters(dot: AnimatedDot, delta_time: int = 0, text: str = '', offset: int = 0):
+    offset = offset % len(text)
+    dot.op_set(delta_time, 'letter', text[offset:])
+    dot.op_set(delta_time + offset, 'letter', text[:offset])
+    dot.op_jmp(len(text), 0)
+    return dot
