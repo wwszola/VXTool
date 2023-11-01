a30cb0ec94410135513e3c4f2d610a5ec3dd53bf
wwszola <wwszola0@gmail.com>
2023-11-01 21:51:59 +0100

    include more type hints

--- VXTool/font.py
+++ VXTool/font.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pygame.font as pgfont
 
-def construct_font_name(path: str | Path, ptsize: int, name: str = ""):
+def construct_font_name(path: str | Path, ptsize: int, name: str = "") -> str:
     if isinstance(path, str):
         path = Path(path)
     filename = path.name.split('.')[0]
@@ -16,11 +16,11 @@ class FontInfo:
         self.ptsize: int = ptsize
         if len(name) == 0:
             name = construct_font_name(self.path, self.ptsize)
-        self.name = name
+        self.name: str = name
 
 class FontBank:
     def __init__(self):
-        self._fonts: dict = dict()
+        self._fonts: dict[pgfont.Font] = dict()
         if not pgfont.get_init():
             pgfont.init()
 
@@ -32,5 +32,5 @@ class FontBank:
         for font_info in fonts_info:
             self.load(font_info)
 
-    def get(self, name: str):
+    def get(self, name: str) -> pgfont.Font:
         return self._fonts[name]
