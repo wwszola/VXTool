9f827544019c532ca30f997fdca5d300ed766254
wwszola <wwszola0@gmail.com>
2023-11-03 19:17:03 +0100

    dot rendering functionality from scratch

--- VXTool/render.py
+++ VXTool/render.py
@@ -1,15 +1,13 @@
-from dataclasses import dataclass, field
 from multiprocessing import Queue
 from enum import Flag, auto
+from queue import Empty as QueueEmpty
 
-from pygame import Surface, Rect
-from pygame.font import Font
-from pygame import transform
+from pygame import Surface
 from pygame import SRCALPHA
 
 from pygame._sdl2 import Renderer, Texture
 
-from .core import Color, BLACK, Dot, Buffer
+from .core import Color, Dot
 from .font import FontBank
 
 class RENDER_MSG(Flag):
@@ -22,128 +20,61 @@ class RENDER_MSG(Flag):
     DEFAULT = auto()
     PROCEDURE = NO_CHANGE | CONTINUE
 
-@dataclass
-class TextRender:
-    shape: tuple[int, int]
-    full_res: tuple[int, int]
-    _font_bank: FontBank
-    block_size: tuple[int, int] = None
+class DotRenderer:
+    def __init__(self, font_bank: FontBank, renderer: Renderer):
+        self.font_bank: FontBank = font_bank
+        self.renderer: Renderer = renderer
 
-    backcolor: Color = BLACK
+        self._hash_to_dot: dict[int, Dot] = dict()
+        self._cached_renders: dict[int, Texture] = dict()
+
+        self._new_dots_q: Queue = Queue()
+
+    def register(self, dot: Dot, hash_value: int):
+        plain_dot = dot.variant(Dot) if dot.__class__ != Dot else dot
+        self._hash_to_dot[hash_value] = plain_dot
+
+    def process_queue(self):
+        try:
+            while True:
+                dot, hash_value = self._new_dots_q.get()
+                self.register(dot, hash_value)
+        except QueueEmpty:
+            pass
     
-    cached_renders: dict[int|str, Surface] = field(default_factory = dict, kw_only = True)
-    _hash_to_dot: dict[int, Dot] = field(default_factory=dict, kw_only=True)
-    screen: Texture = field(init = False, default = None)
-    renderer: Renderer = field(kw_only = True)
+    def get_render(self, hash_value: int) -> Texture:
+        if hash_value not in self._hash_to_dot:
+            raise KeyError(f"No dot attributed with hash value {hash_value} has been registered yet")
+        if hash_value not in self._cached_renders:
+            dot = self._hash_to_dot(hash_value)
+            render = self._render(dot)
+            self._cached_renders[hash_value] = render
+        return self._cached_renders[hash_value]
 
-    _render_q: Queue = field(init = False)
-    frames_rendered_count: int = field(init = False, default = 0)
-
-    def __post_init__(self):
-        if self.block_size:
-            self.resize_screen()
-        self.backcolor = Color(self.backcolor)
-        self._render_q = Queue()
-
-    @property
-    def full_size(self) -> tuple[int, int]:
-        return (self.shape[0] * self.block_size[0], self.shape[1] * self.block_size[1])
-
-    def should_resize(self):
-        return (not self.screen and self.block_size) or (self.screen and self.screen.get_size() != self.full_size)
-
-    def resize_screen(self):
-        print(f'Block size: {self.block_size} Full size: {self.full_size}')
-        self.screen = Texture(self.renderer, self.full_size, 32, target=True)
-        self.cached_renders.clear()
-
-    def block_rect(self, pos: tuple[int, int]) -> Rect:
-        return Rect(
-            (pos[0] * self.block_size[0], pos[1] * self.block_size[1]),
-            self.block_size
-        )
-
-    @property
-    def grid_rect(self) -> Rect:
-        return Rect((0, 0), self.shape)
-
-    def get_dot_size(self, dot: Dot):
-        return self._font_bank.get(dot.font_name).size(dot.letter)
-
-    def _gen_dot_render(self, dot: Dot) -> Surface:
-        block_render = Surface(self.block_size, SRCALPHA)
-        block_rect = block_render.get_rect()
-
-        backcolor = dot.backcolor 
-        if not dot.clear and not dot.backcolor:
-            backcolor = Color(0, 0, 0, 0)
-        if dot.clear and not dot.backcolor:
-            backcolor = self.backcolor
-        block_render.fill(backcolor)
-
+    def _render(self, dot: Dot):
         font = self._font_bank.get(dot.font_name)
 
-        dot_render = font.render(dot.letter, False, dot.color)
-        dot_render.set_alpha(dot.color.a)
-        
-        align = dot.align.strip().lower()
-        match align:
-            case 'stretch':
-                dot_render = transform.scale(dot_render, block_rect.size)
-                rect = block_rect
-            case 'center' | _:
-                rect = dot_render.get_rect(center = block_rect.center)
+        face = font.render(dot.letter, False, dot.color)
+        face.set_alpha(dot.color.a)
 
-        block_render.blit(dot_render, rect)
-        return Texture.from_surface(self.renderer, block_render)
+        size = face.get_size()
+        render = Surface(size, SRCALPHA, 32)
+        backcolor = dot.backcolor 
+        if not dot.backcolor:
+            backcolor = Color(0, 0, 0, 0)
+        render.fill(backcolor)
 
-    def _render_next(self, block = True, timeout: float = None):
-        entry = self._render_q.get(block, timeout)
-        # print(f"TEXTRENDER ENTRY: {entry}")
-        flags = entry[0]
-        args = list(entry[1:])
+        render.blit(face, (0, 0))
+        return Texture.from_surface(self.renderer, render)
 
-        if flags & RENDER_MSG.SET_BLOCK_SIZE:
-            value = args.pop(0)
-            if isinstance(value, Dot):
-                block_size = self.get_dot_size(value)
-                self.block_size = block_size
-            else:
-                self.block_size = value
-            self.resize_screen()
+class DotRendererProxy:
+    def __init__(self, dot_renderer: DotRenderer):
+        self._registered_hashes: set[int] = set()
+        self._new_dots_q: Queue = dot_renderer._new_dots_q
 
-        if flags & RENDER_MSG.REGISTER_DOTS:
-            new_dots = args.pop(0)
-            for _hash, dot in new_dots:
-                self._hash_to_dot[_hash] = dot
-
-        if not flags & RENDER_MSG.NO_CHANGE:
-            data = args.pop(0)
-            data_it = iter(data)
-            blits = []
-            while True:
-                try:
-                    pos = next(data_it)
-                    rect = self.block_rect(pos)
-                    length = next(data_it)
-                    for _ in range(length):
-                        _hash = next(data_it)
-                        if _hash not in self.cached_renders:
-                            dot = self._hash_to_dot[_hash]
-                            self.cached_renders[_hash] = self._gen_dot_render(dot)
-                        blits.append((self.cached_renders[_hash], rect))
-                except StopIteration:
-                    break        
-
-            self.renderer.target = self.screen
-            if not flags & RENDER_MSG.NO_CLEAR:
-                self.renderer.draw_color = self.backcolor
-                self.renderer.clear()
-            for render, rect in blits:
-                self.renderer.blit(render, rect)
-
-        if flags & RENDER_MSG.NO_CHANGE or flags & RENDER_MSG.CONTINUE:
-            return flags
-
-        self.frames_rendered_count += 1
-        return flags
+    def register(self, dot: Dot, hash_value: int):
+        if hash_value in self._registered_hashes:
+            return
+        plain_dot = dot.variant(Dot) if dot.__class__ != Dot else dot
+        self._new_dots_q.put((plain_dot, hash_value))
+        self._registered_hashes.add(hash_value)
